3f535d66a99b91d9379eb7567c28cc564ec3ce90
Koki Hasegawa <116789423+KokiHasegawaEspinal@users.noreply.github.com>
2026-06-23 15:39:37 +0900

    Add first Python loop exercise

---
+++ Bucles/Ejercicio1.py
@@ -0,0 +1,7 @@
+#Ejercicio 1
+#Escribir un programa que pida al usuario una palabra y la muestre por pantalla 10 veces.
+
+word = input ("Escribe una palabra: ")
+
+for i in range(10):
+    print (word)
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # practicas_python
 Desarrolllo logico para aprender Python
+
+## https://aprendeconalf.es/docencia/python/ejercicios/bucles/
+
+Ejercicios de `bucles`
