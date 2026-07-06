80ec4c465d92bebe90836cfb9749aabff2f376ec
Koki Hasegawa <jorge.hasegawa.espinal@gmail.com>
2026-07-06 23:56:03 +0900

    Add Ejercicio 10 de Python chain exercise

---
+++ EjercicioDeCadenas/Ejercicio10.py
@@ -0,0 +1,13 @@
+#Ejercicio 10
+#Escribir un programa que pregunte por consola por los productos
+#de una cesta de la compra, separados por comas, y muestre
+#por pantalla cada uno de los productos en una línea distinta.
+
+producto_cesta = input("Ingrese los productos: ")
+
+productos = producto_cesta.split(",")
+
+print(productos)
+
+for producto in productos:
+    print(producto)
