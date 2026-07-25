bc62227d7045bdeeb2416e5ac9ceb61b5e3e4d97
Koki Hasegawa <jorge.hasegawa.espinal@gmail.com>
2026-07-25 14:32:40 +0900

    Add Ejercicio 6 de Ejercicios de Condicionales

--- EjerciciosDeCondicionales/Ejercicio6.py
+++ EjerciciosDeCondicionales/Ejercicio6.py
@@ -6,6 +6,36 @@
 #Escribir un programa que pregunte al usuario su nombre y sexo,
 #y muestre por pantalla el grupo que le corresponde.
 
-ingrese_nombre = ("Ingresa tu nombre: ")
-ingrese_sexo = ("Ingresa tu genero: ")
+grupo = {
+    "A": "grupo A",
+    "B": "grupo B"
+}   
 
+nombre = input("Ingresa tu nombre: ").upper()
+sexo = input("Ingresa tu genero (M/F):").upper()
+
+#print(nombre)
+#print(sexo)
+#print (nombre.title())
+
+if sexo == "F" and nombre[0] < "M":
+    grupo_print=grupo['A']
+#    print(f"{nombre.title()} pertenece al {grupo['A']}")
+elif sexo == "F" and nombre[0] >= "M":
+    grupo_print=grupo['B']
+#    print(f"{nombre.title()} pertenece al {grupo ['B']}")
+elif sexo == "M" and nombre[0] >"N":
+    grupo_print=grupo['A']
+#    print(f"{nombre.title()} pertenece al {grupo ['A']}")
+else:
+    grupo_print=grupo['B']
+#    print(f"{nombre.title()} pertenece al {grupo ['B']}")
+
+print(f"{nombre.title()} pertenece al {grupo_print}")
+
+    
+    
+    
+    
+    
+    
